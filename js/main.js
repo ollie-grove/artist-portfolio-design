b5f9c818149cef597bf82d27204e9bddd4350135
Ollie Grove <ollie_grove@protonmail.com>
2022-11-07 15:16:08 -0800

    Added a contact form, doesn't work fully yet. Added other sections and navigation links

--- js/main.js
+++ js/main.js
@@ -17,3 +17,55 @@ $(document).ready(function() {
     });
 });
 
+function validateForm() {
+    const name = document.forms["contactForm"]["name"]; 
+    const pronouns = document.forms["contactForm"]["pronouns"];
+    const email = document.forms["contactForm"]["email"]; 
+    const project = document.forms["contactForm"]["project"]; 
+
+    if (name.value == "") {
+        document.querySelector('errorname').innerHTML="Please enter your name";
+        name.focus();
+        return false;
+    } else {
+        document.querySelector('errorname').innerHTML=""; 
+    }
+
+    if (pronouns.value == "") {
+        document.querySelector('errorpronouns').innerHTML="Please specify your pronouns. Example) She/Her, They/Them"; 
+        pronouns.focus();
+        return false; 
+    } else {
+        document.querySelector('errorpronouns').innerHTML=""; 
+    }
+
+    if (email.value == "") {
+        document.querySelector('erroremail').innerHTML="Please enter a valid email address";
+        email.focus();
+        return false;
+    } else {
+        document.querySelector('erroremail').innerHTML=""; 
+    }
+
+    if (email.value.indexOf("@", 0) < 0) {
+        document.querySelector('erroremail').innerHTML="Please enter a valid email address";
+        email.focus(); 
+        return false; 
+    }
+
+    if (email.value.indexOf(".", 0) < 0) {
+        document.querySelector('erroremail').innerHTML="Please enter a valid email address"; 
+        email.focus(); 
+        return false; 
+    }
+
+    if (project.value == "") {
+        document.querySelector('errormsg').innerHTML="Please enter a valid message"; 
+        project.focus();
+        return false; 
+    } else {
+        document.querySelector('errormsg').innerHTML="";
+    }
+    return true;
+    
+}
